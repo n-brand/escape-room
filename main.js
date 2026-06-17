298fb0f5c63d7443c5b07308e4a5809552ea3320
emilijan.russ <emilijan.russ@edu.tbz.ch>
2026-06-17 13:43:48 +0200

    Hintegrund ändern, Divs für 2 Räume hinzufügen und Hover effekt einfügen

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-const scenes = [
-    {
-        img: "/images/bild.png",
-        name: "attic"
-    }
-]
+const rooms = {
+    schlafzimmer: {
+        positionx: 10,
+        positiony: -100,
+        width: 17%,
+        height: 48%
+    },
 
+    schlafzimmerA: {
+        positionx: 10,
+        positiony: -100,
+        width: 17%,
+        height: 48%
+    },
+};
+
+let background_img = document.getElementById()
+
+function onClickBox() {
+    background_img.src = "images/backgrounds/"
+
+}
+
+function onRoom1Clicked() {
+
+}
