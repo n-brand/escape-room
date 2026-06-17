0081691e0355d26e8da26f3826b7273354b42d5f
emilijan.russ <emilijan.russ@edu.tbz.ch>
2026-06-17 14:17:58 +0200

    Raum Ansicht hbinzufügen

--- main.js
+++ main.js
@@ -2,25 +2,39 @@ const rooms = {
     schlafzimmer: {
         positionx: 10,
         positiony: -100,
-        width: 17%,
-        height: 48%
+        width: 17,
+        height: 48
     },
 
     schlafzimmerA: {
         positionx: 10,
         positiony: -100,
-        width: 17%,
-        height: 48%
+        width: 17,
+        height: 48
     },
 };
 
-let background_img = document.getElementById()
+let background_img = document.getElementById("background-img")
+let room1 = document.getElementsByClassName("room-1")
+let room2 = document.getElementsByClassName("room-2")
 
-function onClickBox() {
-    background_img.src = "images/backgrounds/"
 
+function hideRooms() {
+    room1.style.display = "none"
+    room2.style.display = "none"
+}
+function onClickRoom1() {
+    background_img.src = "images/backgrounds/detailansicht_schlafzimmer1.png"
+    hideRooms()
 }
 
-function onRoom1Clicked() {
-
+function onClickRoom2() {
+    background_img.src = "images/background/detailansicht_schlafzimmer1.png"
+    console.log("Hallo2")
 }
+
+function goBack() {
+    background_img.src = "images/backgrounds/house_background.png"
+}
+
+fu
